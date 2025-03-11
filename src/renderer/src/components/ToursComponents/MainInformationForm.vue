8aaea7c91863b6af982bff36b3f3b668039359c9
wombat1742 <tomgogard@gmail.com>
2025-03-11 13:50:30 +0300

    new file:   src/renderer/src/components/ToursComponents/MainInformationForm.vue

---
+++ src/renderer/src/components/ToursComponents/MainInformationForm.vue
@@ -0,0 +1,9 @@
+<template>
+        <label>  
+            Дата начала периода
+            <DatePicker />
+        </label>
+</template>
+<script lang="ts" setup>
+    import DatePicker from 'primevue/datepicker';
+</script>
